7b50d5785ecc7a16e1296da2e7123411be3fa537
binishaaa <binishawale@gmail.com>
2020-11-07 21:58:15 +0545

    Second session assignment

---
+++ assignment2.py
@@ -0,0 +1,39 @@
+# QNO1
+list=[6,9,7,5,4]
+sum=0
+for i in list:
+    sum=sum+i
+print("sum of all elements in a list is",sum)
+
+#QNO2
+a = [1, 1, 3, 5, 7, 9, 9]
+b = [2, 1, 6 ,9, 2, 1, 3, 5]
+set_a=set(a)
+set_b=set(b)
+common=set_a.intersection(set_b)
+common_list=list(common)
+print("common elements in list are:",common_list)
+
+#QNO3
+# list=[1, 5, 7, 12 ,15]
+# for i in range(len(list)):
+#     a=list[i]
+#     print(a)
+#     sq=a**2
+#     print("square of",a,"is",sq)
+
+
+#QNO4
+# s=0
+# a=input("enter any string")
+# print("String is",a)
+# for i in a:
+#     s=s+1
+#     print(i,s)
+# print("total length of string is",s)
+
+
+# QNO5
+# a=input("enter any string")
+# print("Uppercase:",a.upper())
+# print("Lowercase:",a.lower())
